44dd8f88584b287eda7b93288b4523ee69d515c2
Kaizaro <kaizaro78@gmail.com>
2021-04-08 14:43:23 +0300

    add new AsyncStorage

--- src/screens/PostsList.js
+++ src/screens/PostsList.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
+import {AppState, FlatList, StyleSheet, View} from 'react-native';
 
-import {AppState, AsyncStorage, FlatList, StyleSheet, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Header from '../components/common/Header';
 import PostsListItem from '../components/screen_postslist/PostsListItem';
 import {NavigationActions, StackActions} from 'react-navigation';
 import ProgressIndicator from '../components/common/ProgressIndicator';
 import {
-    KEY_FEED_URL_ASYNCSTORAGE,
-    KEY_NUMBER_OF_POSTS_ASYNCSTORAGE,
-    KEY_UPDATE_INTERVAL_ASYNCSTORAGE
-} from "../constants/constants";
-
+  KEY_FEED_URL_ASYNCSTORAGE,
+  KEY_NUMBER_OF_POSTS_ASYNCSTORAGE,
+  KEY_UPDATE_INTERVAL_ASYNCSTORAGE,
+} from '../constants/constants';
 
 const locale = require('../assets/langs/en.json');
 
@@ -24,129 +24,122 @@ let sinceId = null;
 const LOG_TAG = '>>> PostsList ';
 
 class PostsList extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            appState: AppState.currentState,
-            showProgress: true,
-            posts: []
-        };
-    }
-
-    componentDidMount() {
-        AppState.addEventListener('change', this._handleAppStateChange);
-        this.getConfig().then(()=> {
-            this.fetchPosts();
-            this.interval = setInterval(() =>
-                    this.fetchPosts(),
-                updateInterval
-            );
-        });
-    }
-
-    componentWillUnmount(){
-        AppState.removeEventListener('change', this._handleAppStateChange);
-        clearInterval(this.interval);
-    }
-
-    _handleAppStateChange = (nextAppState) => {
-        if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-            this.interval = setInterval(() =>
-                    this.fetchPosts(),
-                updateInterval
-            );
-        } else {
-            clearInterval(this.interval);
-        }
-        this.setState({appState: nextAppState});
+    this.state = {
+      appState: AppState.currentState,
+      showProgress: true,
+      posts: [],
     };
+  }
 
-    async getConfig() {
-        feedUrl = await AsyncStorage.getItem(KEY_FEED_URL_ASYNCSTORAGE);
-        numberOfPosts = JSON.parse(await AsyncStorage.getItem(KEY_NUMBER_OF_POSTS_ASYNCSTORAGE));
-        updateInterval = parseInt(await AsyncStorage.getItem(KEY_UPDATE_INTERVAL_ASYNCSTORAGE))*1000;
-        console.log(LOG_TAG, feedUrl, numberOfPosts, updateInterval);
-    };
+  componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
+    this.getConfig().then(() => {
+      this.fetchPosts();
+      this.interval = setInterval(() => this.fetchPosts(), updateInterval);
+    });
+  }
 
-    async fetchPosts() {
-        this.setState({
-            showProgress: true
-        });
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+    clearInterval(this.interval);
+  }
 
-        try {
-            const response = await fetch(`${feedUrl}?limit=${numberOfPosts}?since_id=${sinceId}`);
-            if (response.status >= 200 && response.status < 300) {
-                //Handle success
-                let data = await response.json();
-                this.setState({
-                    posts: data
-                });
-                sinceId = data[0].entity_id;
-            } else {
-                //Handle error
-            }
-        } catch
-            (error) {
-            console.log(LOG_TAG, 'fetchPosts catch', error);
-        } finally {
-            this.setState({
-                showProgress: false
-            })
-        }
+  _handleAppStateChange = nextAppState => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      this.interval = setInterval(() => this.fetchPosts(), updateInterval);
+    } else {
+      clearInterval(this.interval);
     }
+    this.setState({appState: nextAppState});
+  };
 
-    navigateTo = (routeName) => {
-        const navigateAction = StackActions.reset({
-            index: 0,
-            actions: [NavigationActions.navigate({routeName: routeName})]
-        });
-
-        this.props.navigation.dispatch(navigateAction);
-    };
-    
-
-    keyExtractor = (item, index) => item.id.toString();
-
-    renderItem = ({item}) => (
-        <PostsListItem
-            id={item.id}
-            post={item}/>
+  async getConfig() {
+    feedUrl = await AsyncStorage.getItem(KEY_FEED_URL_ASYNCSTORAGE);
+    numberOfPosts = JSON.parse(
+      await AsyncStorage.getItem(KEY_NUMBER_OF_POSTS_ASYNCSTORAGE),
     );
+    updateInterval =
+      parseInt(await AsyncStorage.getItem(KEY_UPDATE_INTERVAL_ASYNCSTORAGE)) *
+      1000;
+    console.log(LOG_TAG, feedUrl, numberOfPosts, updateInterval);
+  }
 
-    render() {
-        return (
-            <View style={styles.rootStyle}>
-                <Header header={locale.data.postslists_header}/>
-                <FlatList
-                    contentContainerStyle={styles.contentContainer}
-                    data={this.state.posts}
-                    extraData={this.state}
-                    keyExtractor={this.keyExtractor}
-                    renderItem={this.renderItem}
-                    updateCellsBatchingPeriod={75}
-                    maxToRenderPerBatch={13}
-                    initialNumToRender={50}
-                    windowSize={21}/>
-                {this.state.showProgress &&
-                <ProgressIndicator/>
-                }
-            </View>
+  async fetchPosts() {
+    this.setState({
+      showProgress: true,
+    });
 
-        );
+    try {
+      const response = await fetch(
+        `${feedUrl}?limit=${numberOfPosts}?since_id=${sinceId}`,
+      );
+      if (response.status >= 200 && response.status < 300) {
+        //Handle success
+        let data = await response.json();
+        this.setState({
+          posts: data,
+        });
+        sinceId = data[0].entity_id;
+      } else {
+        //Handle error
+      }
+    } catch (error) {
+      console.log(LOG_TAG, 'fetchPosts catch', error);
+    } finally {
+      this.setState({
+        showProgress: false,
+      });
     }
+  }
+
+  navigateTo = routeName => {
+    const navigateAction = StackActions.reset({
+      index: 0,
+      actions: [NavigationActions.navigate({routeName: routeName})],
+    });
+
+    this.props.navigation.dispatch(navigateAction);
+  };
+
+  keyExtractor = (item, index) => item.id.toString();
+
+  renderItem = ({item}) => <PostsListItem id={item.id} post={item} />;
+
+  render() {
+    return (
+      <View style={styles.rootStyle}>
+        <Header header={locale.data.postslists_header} />
+        <FlatList
+          contentContainerStyle={styles.contentContainer}
+          data={this.state.posts}
+          extraData={this.state}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+          updateCellsBatchingPeriod={75}
+          maxToRenderPerBatch={13}
+          initialNumToRender={50}
+          windowSize={21}
+        />
+        {this.state.showProgress && <ProgressIndicator />}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    rootStyle: {
-        flex: 1
-    },
-    contentContainer: {
-        justifyContent: 'flex-start',
-        alignItems: 'stretch'
-    }
+  rootStyle: {
+    flex: 1,
+  },
+  contentContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
 });
-
 
 export default PostsList;

41482f62c71ae9b6a8d1e142c16c1ae158bec337
Kaizaro <kaizaro78@gmail.com>
2021-04-08 15:08:47 +0300

    update config

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,4 +1,9 @@
-export const DEFAULT_API_URL = 'http://api.massrelevance.com/MassRelDemo/kindle.json';
+// deprecated url
+// export const DEFAULT_API_URL = 'http://api.massrelevance.com/MassRelDemo/kindle.json';
+
+// actual url on mocky
+export const DEFAULT_API_URL =
+  'https://run.mocky.io/v3/a78c9189-5570-46b2-8a6a-e4d23ee7475e';
 
 export const DEFAULT_NUMBER_OF_POSTS = 10;
 
